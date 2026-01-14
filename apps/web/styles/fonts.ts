318a9ec7fd396903a92c406107227a2f206b63dc
優 <rainbowstar2400@gmail.com>
2026-01-14 14:42:24 +0900

    10

--- apps/web/styles/fonts.ts
+++ apps/web/styles/fonts.ts
@@ -1,8 +1,4 @@
-import { Inter, Noto_Sans_JP } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 export const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' });
-export const fontRounded = Noto_Sans_JP({
-  subsets: ['latin', 'japanese'],
-  weight: ['400', '500', '700'],
-  variable: '--font-rounded',
-});
+export const fontRounded = { className: 'font-rounded' };
