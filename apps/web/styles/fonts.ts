2cc18f9c06aa2d52e391023a6f0f809f3938df82
優 <rainbowstar2400@gmail.com>
2026-01-14 14:45:36 +0900

    11

--- apps/web/styles/fonts.ts
+++ apps/web/styles/fonts.ts
@@ -1,4 +1,8 @@
 import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
 export const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' });
-export const fontRounded = { className: 'font-rounded' };
+export const fontRounded = localFont({
+  src: [{ path: '../public/fonts/KiwiMaru-Regular.woff2', weight: '400', style: 'normal' }],
+  display: 'swap',
+});
